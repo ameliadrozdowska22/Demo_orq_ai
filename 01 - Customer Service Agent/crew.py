eb6769de42a0a296e820305807cbcc77c8972a07
durenajafamjad <durenajafamjad321@gmail.com>
2024-10-18 11:56:31 +0200

    Removed Secrets and Added a Readme file

--- 01 - Customer Service Agent/crew.py
+++ 01 - Customer Service Agent/crew.py
@@ -4,7 +4,8 @@ import openai
 from openai import OpenAI
 from langchain_openai import ChatOpenAI
 
-openai.api_key = "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"
+import os
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 client = OpenAI(
     api_key = openai.api_key,
@@ -12,7 +13,7 @@ client = OpenAI(
 GPT_MODEL = "gpt-4o-mini"
 
 # manager_llm
-manager_llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=0, openai_api_key="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")
+manager_llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=0, openai_api_key=os.getenv("OPENAI_API_KEY"))
 
 @CrewBase
 class CustomerServiceCrew:
